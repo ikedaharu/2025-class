ff8931d306d6625d67b68d6456230a118abec5dd
LAPTOP-1EDC8BOU\haru <ikedaharu.work.1004@gmail.com>
2025-05-28 15:34:47 +0900

    授業中変更

--- Click-number/js/scripts.js
+++ Click-number/js/scripts.js
@@ -1,31 +1,32 @@
-for (var num=9;num>0;num--) {
+for (var num = 9; num > 0; num--) {
 	var elm = document.createElement("button");
 	elm.innerHTML = num;
-	elm.setAttribute("id", num); 
-	elm.setAttribute("class", "circle"); 
-	var function_name = "remove(" + num + ")" 
+	elm.setAttribute("id", num);
+	elm.setAttribute("class", "circle");
+	var function_name = "remove(" + num + ")";
 	elm.setAttribute("onclick", function_name);
 	document.getElementById("main").appendChild(elm);
-
-	var left = 10;
-	var top = 100;
-
-	left = left + Math.floor(Math.random() * 400);
-	top = top + Math.floor(Math.random() * 600);
-
-	document.getElementById(num).style.left = "" + left + "px" ;
-	document.getElementById(num).style.top = "" + top + "px" ;
-
-	var radius = 30 + Math.floor(Math.random() * 100);
-	document.getElementById(num).style.height = "" + radius + "px";
-	document.getElementById(num).style.width = "" + radius + "px";
-	document.getElementById(num).style.borderRadius = "" + radius + "px";
 }
 
 var next = 1;
-document.remove = function (id) {
+document.remove = function(id) {
 	if (id === next) {
 		document.getElementById("main").removeChild(document.getElementById(id));
 		next = next + 1;
 	}
+};
+
+move();
+function move() {
+	for (var num = 1; num < 10; num++) {
+		var left = 10;
+		var top = 100;
+
+		left = left + Math.floor(Math.random() * 400);
+		top = top + Math.floor(Math.random() * 600);
+
+		document.getElementById(num).style.left = "" + left + "px";
+		document.getElementById(num).style.top = "" + top + "px";
+		}
+	setTimeout(move, 5000);
 }
--- omikuji/js/scripts.js
+++ omikuji/js/scripts.js
@@ -1,6 +1,36 @@
 document.getElementById('draw').addEventListener('click', function () {
     const omikujiResults = [
         '大吉','吉', '中吉', '小吉', '末吉', '凶','大凶'];
-    const result = omikujiResults[Math.floor(Math.random() * omikujiResults.length)]; 
+    const result = omikujiResults[Math.floor(Math.random() * omikujiResults.length)];
+    
+    const resultElement = document.getElementById('result');
+    
     document.getElementById('result').textContent = `あなたの運勢は... ${result}!`;
 });
+
+// 文字色を変更
+  switch (result) {
+    case '大吉':
+      resultElement.style.color = 'red';
+      break;
+    case '吉':
+      resultElement.style.color = 'orange';
+      break;
+    case '中吉':
+      resultElement.style.color = 'gold';
+      break;
+    case '小吉':
+      resultElement.style.color = 'green';
+      break;
+    case '末吉':
+      resultElement.style.color = 'blue';
+      break;
+    case '凶':
+      resultElement.style.color = 'purple';
+      break;
+    case '大凶':
+      resultElement.style.color = 'black';
+      break;
+    default:
+      resultElement.style.color = 'black'; // デフォルトは黒
+  }

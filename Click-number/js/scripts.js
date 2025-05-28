e0d6ab0a1240d040e7e12212d4edaf5b8f3ebdac
LAPTOP-1EDC8BOU\haru <ikedaharu.work.1004@gmail.com>
2025-05-28 14:48:50 +0900

    2025/5/28授業内容中間プッシュ

---
+++ Click-number/js/scripts.js
@@ -0,0 +1,31 @@
+for (var num=9;num>0;num--) {
+	var elm = document.createElement("button");
+	elm.innerHTML = num;
+	elm.setAttribute("id", num); 
+	elm.setAttribute("class", "circle"); 
+	var function_name = "remove(" + num + ")" 
+	elm.setAttribute("onclick", function_name);
+	document.getElementById("main").appendChild(elm);
+
+	var left = 10;
+	var top = 100;
+
+	left = left + Math.floor(Math.random() * 400);
+	top = top + Math.floor(Math.random() * 600);
+
+	document.getElementById(num).style.left = "" + left + "px" ;
+	document.getElementById(num).style.top = "" + top + "px" ;
+
+	var radius = 30 + Math.floor(Math.random() * 100);
+	document.getElementById(num).style.height = "" + radius + "px";
+	document.getElementById(num).style.width = "" + radius + "px";
+	document.getElementById(num).style.borderRadius = "" + radius + "px";
+}
+
+var next = 1;
+document.remove = function (id) {
+	if (id === next) {
+		document.getElementById("main").removeChild(document.getElementById(id));
+		next = next + 1;
+	}
+}
